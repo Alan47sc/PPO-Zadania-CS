c25614e3c18758dc9510c4b378da009533e50714
Alan <aakawa47@gmail.com>
2025-03-27 11:33:29 +0100

    Zadanie 1 - prosty kalkulator

--- Program.cs
+++ Program.cs
@@ -3,8 +3,35 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {  
+            Console.WriteLine("\tProsty Kalkulator Dwóch Liczb");
+
+            Console.WriteLine("\nPodaj pierwszą liczbę: ");
+            var liczba1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę: ");
+            var liczba2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Jaką operację chcesz wykonać:" +
+                "\n1. Dodawanie" +
+                "\n2. Odejmowanie" +
+                "\n3. Mnożenie" +
+                "\n4. Dzielenie");
+            var wybor = int.Parse(Console.ReadLine());
+
+            if (wybor == 1)
+                Console.WriteLine(liczba1 + " + " + liczba2 + " = " + (liczba1 + liczba2));
+            else if (wybor == 2)
+                Console.WriteLine(liczba1 + " - " + liczba2 + " = " + (liczba1 - liczba2));
+            else if (wybor == 3)
+                Console.WriteLine(liczba1 + " * " + liczba2 + " = " + (liczba1 * liczba2));
+            else if (wybor == 4 && liczba2 != 0)
+                Console.WriteLine(liczba1 + " / " + liczba2 + " = " + (liczba1 / liczba2));
+            else
+                Console.WriteLine("Nieprawidłowy wybór.");
+
+
+
         }
     }
 }
--- obj/Debug/net8.0/PPO-C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/PPO-C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPO-C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dbca34ccab3f9638cb589656ebd2d6b9918ded")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPO-C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPO-C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
